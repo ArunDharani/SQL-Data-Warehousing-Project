8efc2c8f9e64c8c5902bad6e2226eebae5efbd45
ArunDharani <myimportantworks2@gmail.com>
2026-07-23 17:38:41 +0530

    created new schema and test scripts

---
+++ Tests/connectiontest/test1.sql
@@ -0,0 +1,7 @@
+select DATABASE();
+
+create DATABASE IF NOT EXISTS DatawarehouseProject;
+
+use DatawarehouseProject;
+
+show SCHEMAS;
---
+++ schemaFolders/createSchema.sql
@@ -0,0 +1,7 @@
+-- Let us create new schema for the datawarehouse project. 
+
+CREATE SCHEMA IF NOT EXISTS NewBronze;
+
+Create SCHEMA IF NOT EXISTS NewSilver;
+
+create SCHEMA IF NOT EXISTS NewGold;
